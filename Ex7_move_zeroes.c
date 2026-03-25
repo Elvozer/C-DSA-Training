ee79046bb0f34bbf526d51000e9a150c3d45424a
Elvozer <tranhuyhoangk6@gmail.com>
2026-03-25 09:22:07 +0700

    Fix: Add real code for Ex7

--- Ex7_move_zeroes.c
+++ Ex7_move_zeroes.c
@@ -1,3 +1,27 @@
 #include<stdio.h>
 
+void move_zeroes(int *A, int n){
+    int *fir = A;
+    int *sec = A;
+    while(sec<A+n){
+    if(*sec!=0){
+        *fir = *sec;
+        fir++;
+        }
+    sec++;
+    }
+    while (fir < A + n) {
+    *fir = 0;
+    fir++;
+        }
+    for (int i=0; i<n; i++){
+        printf("%d ", A[i]);
+        }
+    }
 
+int main(){
+    int A[]={1, 2, 0, 3, 12};
+    int size = sizeof(A)/sizeof(A[0]);
+    move_zeroes(A, size);
+    return 0;
+}
